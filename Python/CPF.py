ecfccaa3f4103c5a9bfef9aa50b0a2264fe89a21
𝓟𝒌 <modderkcaheua@gmail.com>
2024-12-21 11:21:04 -0300

    Quase...

--- Python/CPF.py
+++ Python/CPF.py
@@ -1,30 +1,83 @@
+import os
 import random
 
+while True:
+    os.system('cls')
 
-while exit:
-    opcao = 0 # Opcao 1 para Validar | Opcao 2 para Gerar 
+    opcao = ''
+    cpf_user = ''
     cpf_gerado = ''
+    cpf_multiplicado = 0
+    cpf_gerado_formatado = ''
+    cpf_resto_divisao = ''
+    multiplicacao = 10
+    validador = ''
 
     user = input('\tOque deseja?\n[V]alidar CPF\n[G]erar CPF\n[S]air\n').lower()
     if user[0] == 'v':
         user = input('Digite o CPF:\n')
-        opcao = 1
+        opcao = 'validar'
     elif user[0] == 'g':
-        opcao = 2
+        opcao = 'gerar'
     elif user[0] == 's':
         break
     else:
-        print('Opcao invalida')
+        input('Opcao invalida, aperte qualquer tecla para continuar\n')
         continue
     
-    if opcao == 1:
-
+    if opcao == 'validar':
         for formatar in user:
             if formatar in '0123456789':
-                cpf_gerado += formatar
-        if len(cpf_gerado) != 11:
-            print('CPF Invalido')
+                cpf_user += formatar
+        if len(cpf_user) == 11:
+            cpf_gerado = cpf_user[:9]
+        else:
+            input('CPF Invalido, aperte qualquer tecla para continuar\n')
             continue
-    if opcao == 2:
+
+    if opcao == 'gerar':
         for _ in range(9):
             cpf_gerado += str(random.randint(0, 9))
+
+    for _ in cpf_gerado:
+        cpf_multiplicado += int(_) * multiplicacao
+        multiplicacao -= 1
+
+    cpf_multiplicado *= 10
+
+    cpf_resto_divisao = cpf_multiplicado % 11
+
+    validador = cpf_resto_divisao if cpf_resto_divisao < 10 else 0
+
+    if opcao == 'validar':
+        if str(validador) != cpf_user[9]:
+            input('CPF Invalido, aperte qualquer tecla para continuar\n')
+            continue
+    
+    cpf_gerado += str(validador)
+
+# Verificar/Gerar Segundo digito
+    multiplicacao = 11
+    cpf_multiplicado = 0
+    cpf_resto_divisao = 0
+
+    for _ in cpf_gerado:
+        cpf_multiplicado += int(_) * multiplicacao
+        multiplicacao -= 1
+
+    cpf_multiplicado *= 10
+
+    cpf_resto_divisao = cpf_multiplicado % 11
+
+    validador = cpf_resto_divisao if cpf_resto_divisao < 10 else 0
+
+    if opcao == 'validar':
+        if str(validador) != cpf_user[10]:
+            input('CPF Invalido, aperte qualquer tecla para continuar\n')
+        else:
+            input('CPF Valido, aperte qualquer tecla para continuar\n')
+        continue
+    
+    cpf_gerado += str(validador)
+
+    input(f'CPF gerado: {cpf_gerado}\nAperte qualquer tecla para continuar\n')
